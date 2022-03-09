3c68d900473027a037f6dd0f784b80314fafdc37
Ayaz <ayaz.haque@aliman.vic.edu.au>
2022-03-09 11:42:18 +1100

    add workshop 4 file with practicing numbers,

--- workshop_4.py
+++ workshop_4.py
@@ -1,14 +1,11 @@
-my_name = 'Ayaz'
-my_age = 29
-my_height = 180
-my_weight = 85
-my_eyes = 'Black'
-my_teeth = 'White'
-my_hair = 'Black'
 
-print(f'lets talk about {my_name}.')
 
-age = int(input ('Whats your age? '))
+side_length = input('Please enter the length of side in meters: ')
+area = 6 * int(side_length) * int(side_length)
+print(f'Surface area of the cube is: {area} sqm')
 
-print(f"your age is {age *3}")
+
+
+
+
 
