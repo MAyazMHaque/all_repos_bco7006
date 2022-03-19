b3fbff6610269f97ce844557b60965ae63e98569
Muhammad Ayaz M Haque <100340746+MAyazMHaque@users.noreply.github.com>
2022-03-19 12:11:29 +1100

    adding in class activities of 19th march including functions and class

--- Session_7.py
+++ Session_7.py
@@ -83,10 +83,73 @@ this_dict = {
 #     print(states[x])
 
 # exception handling
-try:
-    print(x)
-except NameError:
-    print("Please enter value of variable")
-except:
-    print("Something else is wrong")
+# try:
+#     print(x)
+# except NameError:
+#     print("Please enter value of variable")
+# except:
+#     print("Something else is wrong"
 
+# Function 19/03/2022
+
+# User defined function
+def function():
+    print("I am creating pythyon function")
+
+function()
+
+# lambda function
+x = lambda a : a ** 5
+print(x(7))
+ # with variables
+xy = lambda a, b : a ** b
+print(xy(7,6))
+
+
+
+# return command
+def add(a,b):
+    return a + b
+
+print(add(5,7))
+
+def my_salary (years_of_experience, age):
+    salary = (years_of_experience * age * 100000) / 365
+    return salary
+print(my_salary(4,29))
+
+# arbitrary (*) arguments when we are not sure how many arguments will be entered:
+def fastest_car (*cars):
+    print("my favorite car is ", cars[2])
+
+fastest_car("BMW","Toyota","Audi")
+
+# keyword arguments:
+def my_grades(CBA, BVA):
+    mean_grade = (3 * CBA + 2 * BVA) /2
+    return mean_grade
+final_grade = my_grades(CBA=85, BVA=79)
+print(final_grade)
+
+# ** kwargs: we use double aesterik when we are not sure how many keyword arguemts will be entered
+def my_new_car(**cars):
+    print("my new car with kwargs is", cars["car2"])
+my_new_car(car1="Cool", car2="Hot", car3="Nice")
+
+
+# Using default parameters:
+def citizenship (country = "Australia"):
+    citizen = "I am a citizen of " + country
+    return citizen
+
+print("lets go travelling !", citizenship("Pakistan"))
+
+
+# passing list as a arguments:
+def breakfast (food):
+    for i in food:
+        print("I love ", i)
+
+fav_food = ["bread","Butter","Tea"]
+
+breakfast(fav_food)
---
+++ Session_8.py
@@ -0,0 +1,25 @@
+# object oriented programming
+
+class Student(object):
+    def __init__(self, name, number):
+        self._name = name
+        self._scores = []
+        for count in range(number):
+            self._scores.append(0)
+
+    def getName(self):
+        return self._name
+
+    def setScore(self, i, score):
+        self._scores[i - 1] = score
+
+    def getScore (self, i):
+        return self._scores[i - 1]
+
+    def getAverage (self):
+        return sum(self._scores) / len(self._scores)
+
+    def getHighScore (self):
+        return max(self._scores)
+
+    def __str__(self):
